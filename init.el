5a7c7a694f588283acb273793631ae33230eef3d
D. A. Jakovlev <dm.a.iakovlev@gmail.com>
2025-01-03 17:43:41 +0300

    Add basic settings for Common Lisp

--- init.el
+++ init.el
@@ -57,6 +57,19 @@
   :config
   (ws-butler-global-mode))
 
+;; auto-complete - auto completion
+(use-package auto-complete
+  :config
+  (setq ac-auto-show-menu t)
+  (setq ac-use-comphist t)
+  (setq ac-quick-help-delay 1)
+  (setq ac-quick-help-prefer-pos-tip nil)
+  (define-globalized-minor-mode real-global-auto-complete-mode
+    auto-complete-mode (lambda ()
+                         (if (not (minibufferp (current-buffer)))
+                             (auto-complete-mode 1))))
+  (real-global-auto-complete-mode t))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;;                ;;;
@@ -74,3 +87,34 @@
 (use-package lab-themes
   :config
   (lab-themes-load-style 'light))
+
+;; rainbow-delimiters - colorize parentheses
+(use-package rainbow-delimiters
+  :hook
+  (lisp-interaction-mode . rainbow-delimiters-mode)
+  (emacs-lisp-mode . rainbow-delimiters-mode)
+  (lisp-mode . rainbow-delimiters-mode))
+
+
+;;;;;;;;;;;;;;;;;;;
+;;;             ;;;
+;;; Common Lisp ;;;
+;;;             ;;;
+;;;;;;;;;;;;;;;;;;;
+
+;;; slime - SLIME (Superior Lisp Interaction Mode for Emacs)
+(use-package slime
+  :config
+  (setq inferior-lisp-program "sbcl")
+  (setq slime-load-failed-fasl 'always)
+  (setq slime-net-coding-system 'utf-8-unix)
+  (setq lisp-indent-function 'common-lisp-indent-function)
+  (setq slime-description-autofocus t))
+
+;;; ac-slime - auto completion for SLIME
+(use-package ac-slime
+  :config
+  (add-to-list 'ac-modes 'slime-repl-mode)
+  :hook
+  (slime-mode . set-up-slime-ac)
+  (slime-repl-mode . set-up-slime-ac))
